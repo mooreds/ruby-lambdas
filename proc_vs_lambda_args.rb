d5f7792f7a46e0d4de79a629fbd1a3e33b8310b6
Dan Moore <github@mooreds.com>
2020-03-30 19:31:49 +0000

    more testing of arg numbers

--- proc_vs_lambda_args.rb
+++ proc_vs_lambda_args.rb
@@ -6,6 +6,7 @@ my_lambda.call("jerry")
 
 
 my_proc.call
-my_lambda.call
+#my_lambda.call
 
-# my_lambda.call("jerry","kramer") # more args fail too
+my_proc.call("jerry","kramer") 
+#my_lambda.call("jerry","kramer") # more args fail too
